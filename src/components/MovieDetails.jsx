059cde40238045670974a8cb2eeb29906275e2b8
Bhakthishri S Sharma I <shribhakthi29@gmail.com>
2023-09-07 21:43:14 +0530

    Added  tabs in movie details page

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./MovieDetails.css";
 import SeethaRamam from "../images/bg-image.jpg";
-import Footer from "./Footer";
-
+import { useState } from "react";
 
 const MovieDetails = ({ movieDetails }) => {
+  const [currentTab, setCurrentTab] = useState("overview");
   const genreNames = movieDetails.genres.map((genre) => genre.name);
   const concatenatedGenres = genreNames.join(", ");
   const langs = movieDetails.spoken_languages.map((lang) => lang.name);
@@ -33,51 +33,68 @@ const MovieDetails = ({ movieDetails }) => {
   return (
     <section className="moviedetails">
       <div className="moviedetails-title">
-        <h2 className="moviedetails-tab">OVERVIEW</h2>
-        <h2 className="moviedetails-tab">VIDEOS</h2>
-        <h2 className="moviedetails-tab">PHOTOS</h2>
+        <h2
+          onClick={() => setCurrentTab("overview")}
+          className="moviedetails-tab"
+        >
+          OVERVIEW
+        </h2>
+        <h2
+          onClick={() => setCurrentTab("videos")}
+          className="moviedetails-tab"
+        >
+          VIDEOS
+        </h2>
+        <h2
+          onClick={() => setCurrentTab("photos")}
+          className="moviedetails-tab"
+        >
+          PHOTOS
+        </h2>
       </div>
-      <div className="moviedetails-description">
-        <div className="moviedetails-description-image">
-          <img
-            src={`https://image.tmdb.org/t/p/original${movieDetails.poster_path}`}
-            alt="SeethaRamam"
-            width={320}
-            height={470}
-          />
-        </div>
-        <div className="moviedetails-description-details">
-          <p className="moviedescription-title">Storyline</p>
+      {currentTab === "overview" && (
+        <div className="moviedetails-description">
+          <div className="moviedetails-description-image">
+            <img
+              src={`https://image.tmdb.org/t/p/original${movieDetails.poster_path}`}
+              alt="SeethaRamam"
+              width={320}
+              height={470}
+            />
+          </div>
+          <div className="moviedetails-description-details">
+            <p className="moviedescription-title">Storyline</p>
 
-          <p className="moviedetails-summary">{movieDetails.overview}</p>
-          <div className="moviedetails-description-moviedetails">
-            <div className="moviedetails-description-moviedetails-attributes">
-              <p>Released</p>
-              <p>Runtime</p>
-              <p>Budget</p>
-              <p>Revenue</p>
-              <p>Genres</p>
-              <p>Status</p>
-              <p>Language</p>
-              <p>Production</p>
-            </div>
-            <div className="moviedetails-description-moviedetails-values">
-              <p>{formatDate(movieDetails.release_date)}</p>
-              <p>{convertToHoursAndMinutes(movieDetails.runtime)}</p>
-              <p>${movieDetails.budget}</p>
-              <p>${movieDetails.revenue}</p>
-              <p>{concatenatedGenres}</p>
-              <p>{movieDetails.status}</p>
-              <p>{concatenatedLangs}</p>
-              <p>{concatenatedCompanies} </p>
+            <p className="moviedetails-summary">{movieDetails.overview}</p>
+            <div className="moviedetails-description-moviedetails">
+              <div className="moviedetails-description-moviedetails-attributes">
+                <p>Released</p>
+                <p>Runtime</p>
+                <p>Budget</p>
+                <p>Revenue</p>
+                <p>Genres</p>
+                <p>Status</p>
+                <p>Language</p>
+                <p>Production</p>
+              </div>
+              <div className="moviedetails-description-moviedetails-values">
+                <p>{formatDate(movieDetails.release_date)}</p>
+                <p>{convertToHoursAndMinutes(movieDetails.runtime)}</p>
+                <p>${movieDetails.budget}</p>
+                <p>${movieDetails.revenue}</p>
+                <p>{concatenatedGenres}</p>
+                <p>{movieDetails.status}</p>
+                <p>{concatenatedLangs}</p>
+                <p>{concatenatedCompanies} </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <Footer></Footer>
+      )}
+      {currentTab === "videos" && <h1>Videos</h1>}
+      {currentTab === "photos" && <h1>Photos</h1>}
 
     </section>
-    
   );
 };
 
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -4,17 +4,39 @@ import { useParams } from "react-router-dom";
 import Landing from "../components/Landing";
 import Tools from "../components/Tools";
 import MovieDetails from "../components/MovieDetails";
+import Footer from "./Footer";
+import { CarouselLayout } from "../components/Carousel";
 
-const Movie = ({searchMode, setSearchMode}) => {
+
+const Movie = ({ searchMode, setSearchMode }) => {
   const [movie, setMovie] = useState(null);
   const { id } = useParams();
   const apiKey = "3bf86b4334ec0be302abbf616d7b5e18";
   const [cast, setCast] = useState(null);
-  const [landingMovie, setLandingMovie] = useState(null);
-
 
   const apiUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`;
   const castApiUrl = `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}`;
+  const [similarMovies, setSimilarMovies] = useState(null);
+  const [recommendedMovies, setRecommendedMovies] = useState(null);
+
+  useEffect(() => {
+    // Fetch similar movies
+    fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => setSimilarMovies(data.results))
+      .catch((error) => console.error("Error fetching similar movies:", error));
+
+    // Fetch recommended movies
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => setRecommendedMovies(data.results))
+      .catch((error) =>
+        console.error("Error fetching recommended movies:", error)
+      );
+  }, [id]);
+
   useEffect(() => {
     axios
       .all([axios.get(apiUrl), axios.get(castApiUrl)])
@@ -33,12 +55,31 @@ const Movie = ({searchMode, setSearchMode}) => {
 
   return (
     <>
-      {movie && cast && (
+      {movie && cast && similarMovies && recommendedMovies && (
         <div className="container">
           <Tools setSearchMode={setSearchMode} searchMode={searchMode}></Tools>
           <div className="movie-container">
             <Landing movie={movie}></Landing>
             <MovieDetails movieDetails={movie}></MovieDetails>
+
+            <div className="playing-movies">
+                  <div className="status-explore">
+                    <p style={{color:"white"}} className="movie-status">Similar  movies</p>
+                    
+                  </div>
+                  <CarouselLayout movies={similarMovies}></CarouselLayout>
+
+                </div>
+
+                <div className="status-explore">
+                    <p style={{color:"white"}} className="movie-status">Recommended movies</p>
+                    
+                  </div>
+                  <CarouselLayout movies={recommendedMovies}></CarouselLayout>
+
+
+
+            <Footer></Footer>
           </div>
         </div>
       )}
